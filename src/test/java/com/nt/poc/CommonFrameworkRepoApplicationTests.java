04ee490c2d1b09a87f21a62cbd03fed9fa309e76
shripradhan <40176281+shripradhan@users.noreply.github.com>
2025-04-01 15:53:25 +0530

    Initial commit

---
+++ src/test/java/com/nt/poc/CommonFrameworkRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonFrameworkRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
